fd1a865d5c243042865f7f0fae6ce84b689ee628
gcampbell391 <gcampbell391@gmail.com>
2020-09-08 15:21:01 -0400

    solved sparse search

--- Sorting&Searching/Sparse_Search.js
+++ Sorting&Searching/Sparse_Search.js
@@ -4,3 +4,34 @@
 
 //Given a sorted array of strings that is interspered with empty strings, write 
 //a method to find the location of a given string 
+
+
+const stringArray1 = [
+    "at",
+    "",
+    "",
+    "",
+    "ball",
+    "",
+    "",
+    "car",
+    "",
+    "",
+    "dad",
+    "",
+    ""
+]
+
+const findStringLocation = (strArray, string) => {
+    for (let x = 0; x < strArray.length; x++) {
+        if (strArray[x] === string) {
+            return x
+        }
+    }
+}
+
+
+console.log(findStringLocation(stringArray1, 'at')) //Output: 0
+console.log(findStringLocation(stringArray1, 'dad')) //Output: 10
+console.log(findStringLocation(stringArray1, "ball")) //Output: 4
+console.log(findStringLocation(stringArray1, 'car')) //Output: 7
