aa59334a8603c893dc43c794c32c3dd44c9bd7a0
gcampbell391 <gcampbell391@gmail.com>
2020-09-08 14:20:38 -0400

    solved group anagrams

--- Pinterest_Interview/Hidden_Message.js
+++ Pinterest_Interview/Hidden_Message.js
@@ -1,3 +1,29 @@
+// You are running a classroom and suspect that some of your students are passing around the answers to multiple choice questions disguised as random strings.
+// Your task is to write a function that, given a list of words and a string, finds and returns the word in the list that is scrambled up inside the string, if any exists. There will be at most one matching word. The letters don't need to be contiguous.
+
+// Example:
+
+// words = ['cat', 'dog', 'bird', 'car', 'ax', 'baby']
+
+// string1 = 'tcabnihjs'
+// find_embedded_word(words, string1) -> cat
+
+// string2 = 'tbcanihjs'
+// find_embedded_word(words, string2) -> cat
+
+// string3 = 'baykkjl'
+// find_embedded_word(words, string3) -> None
+
+// string4 = 'bbabylkkj'
+// find_embedded_word(words, string4) -> baby
+
+// n = number of words in `words`
+
+// m = maximal string length of each word
+
+
+
+
 const words = ["cat", "dog", "bird", "car", "ax", "baby"];
 const string1 = "tcabnihjs";
 const string2 = "tbcanihjs";
@@ -6,15 +32,15 @@ const string4 = "bbabylkkj";
 
 const find_embedded_word = (words, str) => {
     let hiddenWord = ""
-    for (let x = 0; x < words.length; x++) {
+    for (let n = 0; n < words.length; n++) {
         let encryptedphrase = str
         hiddenWord = ""
-        for (let y = 0; y < words[x].length; y++) {
-            if (encryptedphrase.includes(words[x][y])) {
-                encryptedphrase = encryptedphrase.replace(words[x][y], "")
-                hiddenWord = hiddenWord + words[x][y]
+        for (let m = 0; m < words[n].length; m++) {
+            if (encryptedphrase.includes(words[n][m])) {
+                encryptedphrase = encryptedphrase.replace(words[n][m], "")
+                hiddenWord = hiddenWord + words[n][m]
             }
-            if (hiddenWord === words[x]) {
+            if (hiddenWord === words[n]) {
                 return hiddenWord
             }
         }
--- Sorting&Searching/Group_Anagrams.js
+++ Sorting&Searching/Group_Anagrams.js
@@ -3,3 +3,56 @@
 //pg 150
 
 //Write a method to sort an array of strings so that all the anagrams are next to each other. 
+
+const strArray1 = [
+    "netb",
+    "moat",
+    "need",
+    "toam",
+    "deen",
+    "atom",
+    "bent"
+]
+
+const strArray2 = [
+    "will",
+    "rake",
+    "bo",
+    "fi",
+    "llwi",
+    "if",
+    "kare",
+    "liwl",
+    "raek"
+]
+
+const alignAnagrams = (array) => {
+    let anagramArray = array
+    //Loop through array of strings
+    for (let x = 0; x < array.length; x++) {
+        // Loop through array to see if there is a match to current string
+        for (let y = 0; y < array.length; y++) {
+            let str1 = array[x]
+            let str2 = array[y]
+            //If the word isn't itself && has the same characters as another string
+            //in the array then remove the match(y) and insert the match behind(x) 
+            if (x !== y && sortString(str1) === sortString(str2)) {
+                anagramArray.splice(y, 1)
+                anagramArray.splice(x, 0, str2)
+            }
+        }
+    }
+    return anagramArray
+}
+
+//Helper function to sort the characters of a string
+//in alphabetical order 
+const sortString = (str) => {
+    let arr = str.split('')
+    let sorted = arr.sort()
+    return sorted.join('')
+}
+
+
+console.log(alignAnagrams(strArray1))
+console.log(alignAnagrams(strArray2))
