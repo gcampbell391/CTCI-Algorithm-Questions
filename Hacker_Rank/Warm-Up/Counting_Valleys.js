fe329258f034004c457262cbd57032028e16ac8c
gcampbell391 <gcampbell391@gmail.com>
2020-09-09 14:54:46 -0400

    hacker rank added

---
+++ Hacker_Rank/Warm-Up/Counting_Valleys.js
@@ -0,0 +1,24 @@
+// Complete the countingValleys function below.
+function countingValleys(n, s) {
+    let currentElevation = 0
+    let valleysCrossed = 0
+    let enteredValley = false
+
+    for (let x = 0; x < n; x++) {
+        if (s[x] === 'U') {
+            currentElevation = currentElevation + 1
+            if (enteredValley === true && currentElevation > -1) {
+                valleysCrossed = valleysCrossed + 1
+                enteredValley = false
+            }
+        }
+        if (s[x] === 'D') {
+            currentElevation = currentElevation - 1
+            if (currentElevation < 0) {
+                enteredValley = true
+            }
+        }
+    }
+    return valleysCrossed
+
+}
