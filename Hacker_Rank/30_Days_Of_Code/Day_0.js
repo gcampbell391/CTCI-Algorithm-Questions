dc8eac364f28bccdd6f396d74d1892cde0a9dc5b
gcampbell391 <gcampbell391@gmail.com>
2020-09-11 13:05:00 -0400

    solved day 0

---
+++ Hacker_Rank/30_Days_Of_Code/Day_0.js
@@ -0,0 +1,12 @@
+/* 
+    Print the param input string after "Hello World" is printed
+*/
+
+
+
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    console.log(inputString)
+    // Write the second line of output that prints the contents of 'inputString' here.
+}
