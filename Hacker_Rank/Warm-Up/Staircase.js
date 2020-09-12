fdc2957df973c76e7f01cf312c33b1c4bee92358
gcampbell391 <gcampbell391@gmail.com>
2020-09-11 21:24:59 -0400

    solved staircase

---
+++ Hacker_Rank/Warm-Up/Staircase.js
@@ -0,0 +1,30 @@
+/*
+Print a staircase of size n using # symbols and spaces.
+
+Ex:
+n = 4
+   #
+  ##
+ ###
+####
+
+Note: The last line must have 0 spaces in it.
+*/
+
+function staircase(n) {
+    const staircase = '#'
+    const extraSpaces = " "
+    let staircaseMessage = ""
+    for (let x = 0; x < n; x++) {
+        staircaseMessage = ""
+        if (x + 1 === n) {
+            staircaseMessage = staircaseMessage + staircase.repeat(x + 1)
+            console.log(staircaseMessage)
+        }
+        else {
+            staircaseMessage = staircaseMessage + extraSpaces.repeat(n - (x + 1))
+            staircaseMessage = staircaseMessage + staircase.repeat(x + 1)
+            console.log(staircaseMessage)
+        }
+    }
+}
